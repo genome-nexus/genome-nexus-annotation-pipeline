ffab84915c0c5211f21d4b9d276afea3f71348c2
Mete Ozguz <ozguz.mete@gmail.com>
2022-09-03 16:08:15 +0300

    Add runtime

--- annotationPipeline/src/main/java/org/cbioportal/annotation/AnnotationPipeline.java
+++ annotationPipeline/src/main/java/org/cbioportal/annotation/AnnotationPipeline.java
@@ -51,6 +51,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,14 +75,14 @@ public class AnnotationPipeline {
 
         Job annotationJob = ctx.getBean(BatchConfiguration.ANNOTATION_JOB, Job.class);
         JobParameters jobParameters = new JobParametersBuilder()
-            .addString("filename", filename)
-            .addString("outputFilename", outputFilename)
-            .addString("outputFormat", outputFormat)
-            .addString("replace", String.valueOf(replace))
-            .addString("isoformOverride", isoformOverride)
-            .addString("errorReportLocation", errorReportLocation)
-            .addString("postIntervalSize", postIntervalSize)
-            .toJobParameters();
+                .addString("filename", filename)
+                .addString("outputFilename", outputFilename)
+                .addString("outputFormat", outputFormat)
+                .addString("replace", String.valueOf(replace))
+                .addString("isoformOverride", isoformOverride)
+                .addString("errorReportLocation", errorReportLocation)
+                .addString("postIntervalSize", postIntervalSize)
+                .toJobParameters();
         JobExecution jobExecution = jobLauncher.run(annotationJob, jobParameters);
         if (!jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)) {
             System.exit(2);
@@ -116,11 +118,13 @@ public class AnnotationPipeline {
     }
 
     public static void main(String[] args) {
+        Instant start = Instant.now();
         try {
             subMain(args);
         } catch (Exception e) {
             LOG.error(e.getMessage());
         }
+        System.out.println("RUNTIME: " + Duration.between(start, Instant.now()).getSeconds() + " secs");
     }
 
     private static void merge(Subcommand subcommand) throws MergeFailedException {
