48d2c3292132d6848ba5c6fe48f660ec8d648c89
Xiang Li <xiangli5699@gmail.com>
2023-02-09 11:01:08 -0500

    Update README.md

--- annotationPipeline/src/main/java/org/cbioportal/annotation/cli/AnnotationFailedException.java
+++ annotationPipeline/src/main/java/org/cbioportal/annotation/cli/AnnotationFailedException.java
@@ -3,7 +3,6 @@ package org.cbioportal.annotation.cli;
 public class AnnotationFailedException extends Exception {
     public AnnotationFailedException(String message) {
         super(message);
-        System.out.println(message);
     }
 
     public AnnotationFailedException(Exception e) {
