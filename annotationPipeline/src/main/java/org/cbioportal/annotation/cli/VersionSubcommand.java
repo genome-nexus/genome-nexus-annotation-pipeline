b3b275bb36e9bfae7aeb02abc97cb00133831f9c
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-07-31 15:32:39 -0400

    Add server version to version sub command

--- annotationPipeline/src/main/java/org/cbioportal/annotation/cli/VersionSubcommand.java
+++ annotationPipeline/src/main/java/org/cbioportal/annotation/cli/VersionSubcommand.java
@@ -3,9 +3,17 @@ package org.cbioportal.annotation.cli;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.genome_nexus.ApiException;
+import org.genome_nexus.client.AggregateSourceInfo;
+import org.genome_nexus.client.InfoControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class VersionSubcommand implements Subcommand {
     private static Options options;
+    private static final String UKNOWN_GENOME_NEXUS_VERSION = "unknown";
+    private final Logger LOG = LoggerFactory.getLogger(VersionSubcommand.class);
 
     static {
         options = getOptions();
@@ -18,6 +26,17 @@ public class VersionSubcommand implements Subcommand {
 
     public VersionSubcommand(String[] args) throws ParseException {
         commandLine = Subcommands.getCommandLine(args, options);
+    }
+
+    public String getServerVersion() {
+        InfoControllerApi infoApiClient = new InfoControllerApi();
+        try {
+            AggregateSourceInfo result = infoApiClient.fetchVersionGET();
+            return result.getGenomeNexus().getServer().getVersion();
+        } catch (ApiException e) {
+            LOG.error("Exception when calling InfoControllerApi#fetchVersionGET, genome nexus version is unknown", e);
+        }
+        return UKNOWN_GENOME_NEXUS_VERSION;
     }
 
     private static Options getOptions() {
